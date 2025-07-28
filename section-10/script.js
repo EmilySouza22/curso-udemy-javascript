11ee3128773fb1bea2c9930c65e5e1fa19dafe9a
EmilySouza22 <emilyseafonso@gmail.com>
2025-07-28 19:21:44 -0300

    adiciona seção de estudos sobre orientção a objetos

---
+++ section-10/script.js
@@ -0,0 +1,10 @@
+/* Section 10: Conceitos de orientação a objetos */
+
+/* 
+    O que é a orientação a objetos?
+
+    - Uma forma de programar, que utiliza os objetos como o seu principal princípio;
+    - Além de utilizar conceitos e técnicas que envolvem objetos;
+    - A maioria dos softwares, na parte de back-end, são desenvolvidos em cima desse paradigma;
+    - Grandes frameworks se aproveitam desta técnica: Laravel, Django e etc;
+*/
