501e7c529c7d82a27cca40c3364e6104beca699f
EmilySouza22 <emilyseafonso@gmail.com>
2025-08-06 17:10:04 -0300

    Estudando: Orientação a Objetos

--- section-10/script.js
+++ section-10/script.js
@@ -8,3 +8,31 @@
     - A maioria dos softwares, na parte de back-end, são desenvolvidos em cima desse paradigma;
     - Grandes frameworks se aproveitam desta técnica: Laravel, Django e etc;
 */
+
+/* 
+    Métodos
+
+    - Propriedade que servem como funções;
+    - Ou seja, as ações dos objetos;
+    - Invocamos os métodos da mesma maneira que funções;
+*/
+
+let cachorro = {
+    latir: function(){
+        console.log("Au au");
+    }
+}
+
+cachorro.latir();
+
+const cachorro2 = {
+    uivar: function(){
+        console.log('Auuuuuuuuuuuuu')
+    },
+    rosnar: function(){
+        console.log('Ggrrrrrrrrrr')
+    }
+}
+
+console.log(cachorro2.uivar());
+console.log(cachorro2.rosnar());
