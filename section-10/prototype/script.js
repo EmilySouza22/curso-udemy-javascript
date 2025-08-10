98b99600262f87a75b400cd8c22601c089d2c5ca
EmilySouza22 <emilyseafonso@gmail.com>
2025-08-09 23:57:36 -0300

    POO: Mais sobre prototype

--- section-10/prototype/script.js
+++ section-10/prototype/script.js
@@ -16,3 +16,24 @@ console.log(Object.getPrototypeOf(pessoa) === Object.prototype);
 
 console.log(pessoa.hasOwnProperty('maos'));
 console.log(pessoa.hasOwnProperty('pes'));
+
+/* 
+    Mais sobre Prototype
+
+    - Quando criamos um objeto a partir de um outro, o base será o prototype;
+    - Ele herdará tanto os métodos e propriedades de Object (o prototype do objeto base)
+    - Quanto os do objeto base para este novo;
+*/
+
+/* Exemplo 2 */
+
+let pessoa2 = {
+	maos: 2,
+};
+
+let pessoaNova = Object.create(pessoa);
+
+console.log(pessoaNova.maos);
+console.log(Object.getPrototypeOf(pessoaNova) == pessoa2);
+
+console.log(pessoaNova.hasOwnProperty('maos'));
